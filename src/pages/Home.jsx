6f90b42e632f229c571f98836dbf617df47ceaf7
Davinderpal Singh Rehal <davinderpalrehal@gmail.com>
2020-12-13 08:42:24 +0300

    Code clean up and removed the radial gradient from Into Blurb

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,12 @@
 import React from 'react'
 import IntroVideo from '../components/IntroVideo'
 import IntroBlurb from '../components/IntroBlurb'
-// import Courses from '../components/Courses'
-// import FeesStructure from '../components/FeesStructure'
-// import RegistrationForm from './RegistrationForm'
 
 function Home () {
   return (
     <>
       <IntroVideo/>
       <IntroBlurb/>
-      {/*<Courses/>*/}
-      {/*<FeesStructure/>*/}
-      {/*<RegistrationForm/>*/}
     </>
   )
 }
